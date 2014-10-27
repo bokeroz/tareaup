78c6ecafde2495661606531cd40d330b8caceaa9
bokeroz <eduardo.marquez.cardona@hotmail.com>
2014-10-27 14:13:56 -0600

    frist

---
+++ module/Modu/src/Modu/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Modu\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+
+    	$view = new ViewModel(array(
+            'message' => 'This is my frist view..........................',
+            'message2' => 'This my message2',
+        ));
+        
+        return $view;
+
+
+       # return new ViewModel();
+    }
+}
